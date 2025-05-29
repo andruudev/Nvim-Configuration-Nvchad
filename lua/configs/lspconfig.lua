338ff095770f1d2fd5c44ca84e8875759d2694a2
andruudev <andruudev@andruudev>
2025-05-29 16:24:51 -0500

    First files

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,15 @@
+-- Carga defaults de NvChad (on_attach y capabilities)
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls" }
-vim.lsp.enable(servers)
+-- Lista de LSPs a habilitar
+local servers = { "html", "cssls", "lua_ls", "ts_ls", "emmet_ls", "emmet_language_server", "golangci_lint_ls", "gopls", "graphql", "grammarly", "htmx", "dockerls", "docker_compose_language_service", "cssmodules_ls", "css_variables", "jsonls", "jsonnet_ls", "kotlin_language_server", "matlab_ls", "rust_analyzer", "tailwindcss", "terraformls", "angularls", "azure_pipelines_ls", "astro"}
 
--- read :h vim.lsp.config for changing options of lsp servers 
+-- Configura mason-lspconfig para instalarlos
+require("mason-lspconfig").setup({
+  ensure_installed = servers,
+})
+
+-- Habilita los LSPs (usando la configuración por defecto de NvChad)
+for _, lsp in ipairs(servers) do
+  require("lspconfig")[lsp].setup({})
+end
